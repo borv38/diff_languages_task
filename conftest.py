9677ac82ccdaee59a5fdff89801a06269239af9f
vvborsce <borv38@gmail.com>
2023-10-24 11:04:18 +0300

    Added

--- conftest.py
+++ conftest.py
@@ -9,10 +9,12 @@ browser = webdriver.Chrome(options=options)
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default=None,
                      help="Choose browser: chrome or firefox")
-
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: ru or fr")
 
 @pytest.fixture(scope="function")
 def browser(request):
+    user_language = request.config.getoption("language")
     browser_name = request.config.getoption("browser_name")
     browser = None
     if browser_name == "chrome":
