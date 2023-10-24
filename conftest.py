8ec9688d36dd551f870feb22230f1f260ec4c9ec
vvborsce <borv38@gmail.com>
2023-10-24 12:43:45 +0300

    Added

--- conftest.py
+++ conftest.py
@@ -3,9 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=chrome,
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=es,
+    parser.addoption('--language', action='store', default='es',
                      help="Choose language: ru/fr/es")
 
 
