9ac26e5adc00a804e1040c05ddfc20d5b925b9ad
vvborsce <borv38@gmail.com>
2023-10-24 11:49:06 +0300

    Added

--- conftest.py
+++ conftest.py
@@ -10,10 +10,10 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browser(request):
+    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser_name = request.config.getoption("browser_name")
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
