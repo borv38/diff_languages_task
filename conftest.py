9bb1383e4cf5f5fc02d4fc46b86e02e3d81a90f0
vvborsce <borv38@gmail.com>
2023-10-24 11:11:11 +0300

    Added

--- conftest.py
+++ conftest.py
@@ -2,8 +2,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
 browser = webdriver.Chrome(options=options)
 
 def pytest_addoption(parser):
@@ -14,7 +12,9 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browser(request):
-    user_language = request.config.getoption("user_language")
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser_name = request.config.getoption("browser_name")
     browser = None
     if browser_name == "chrome":
